188410f7fe2a1a9fa6c9d1734476e4705cd48c2d
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-01-29 13:54:56 +0100

    up

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and use console.log(...) to print each line
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
