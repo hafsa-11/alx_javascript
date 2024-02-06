6a2b4e8d82d9dace0619cc2f06f1078b47c8860a
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-02-06 09:15:13 +0100

    up

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,28 @@
+// Importing the request module
+const request = require('request');
+
+// Function to count the number of movies where the character "Wedge Antilles" is present
+function countMoviesWithWedgeAntilles(apiUrl) {
+    // Sending a GET request to the API URL
+    request.get(apiUrl, function(error, response, body) {
+        if (error) {
+            // If there's an error, print it
+            console.error('Error:', error);
+        } else {
+            // Parsing the response body to extract information about the movies
+            const films = JSON.parse(body).results;
+
+            // Counting the number of movies where "Wedge Antilles" is present
+            const count = films.filter(movie => movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18')).length;
+
+            // Printing the count
+            console.log(count);
+        }
+    });
+}
+
+// Extracting the API URL from the command line arguments
+const apiUrl = process.argv[2];
+
+// Calling the function with the provided API URL
+countMoviesWithWedgeAntilles(apiUrl);
