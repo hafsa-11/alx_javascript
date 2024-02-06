65e26fb41c160547058678359b27c3afb0e4c4de
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-02-06 10:09:26 +0100

    up

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -2,31 +2,28 @@ const request = require('request');
 const fs = require('fs');
 
 // Function to get the contents of a webpage and store it in a file
-function requestAndStore(url, filePath) {
-    // Sending a GET request to the URL
+function requestAndStore(url, fileName) {
     request.get(url, function(error, response, body) {
         if (error) {
-            // If there's an error, print it
             console.error('Error:', error);
-        } else {
-            // Writing the response body to the file
-            fs.writeFile(filePath, body, 'utf-8', function(err) {
-                if (err) {
-                    // If there's an error writing the file, print it
-                    console.error('Error writing file:', err);
-                } else {
-                    // If successful, print a success message
-                    console.log('File saved successfully:', filePath);
-                }
-            });
+            return;
         }
+
+        // Writing the response body to the file
+        fs.writeFile(fileName, body, 'utf-8', function(err) {
+            if (err) {
+                console.error('Error writing file:', err);
+                return;
+            }
+            console.log('File saved successfully:', fileName);
+        });
     });
 }
 
-// Extracting the URL and file path from the command line arguments
+// Extracting URL and file name from command line arguments
 const url = process.argv[2];
-const filePath = process.argv[3];
+const fileName = process.argv[3];
 
-// Calling the function with the provided URL and file path
-requestAndStore(url, filePath);
+// Calling the function with provided URL and file name
+requestAndStore(url, fileName);
 
